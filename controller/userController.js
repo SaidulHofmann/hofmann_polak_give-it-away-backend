2fb2562acbeba75fb3665a8ffef26a18b4ee491c
Martin Polak <martin.polak@hsr.ch>
2018-02-25 00:02:47 +0100

    user api: controller splitet to controller and service (userStore)

--- controller/userController.js
+++ controller/userController.js
@@ -1,22 +1,20 @@
 // User controller that handles article api requests.
 
-const mongoose = require('mongoose');
-const User = require('../models/User');
+
 const _ = require('underscore');
+const store = require('../services/userStore');
 // const util = require("../util/security");
 
 
 
 
 exports.createUser = function (req, res) {
-    let user = new User(req.body);
-    user.save()
-        .then(user => {
-        res.json(user);
-})
-.catch(err => {
-        res.status(400).send("Unable to save the user.");
-});
+
+    let register = store.add(req.body, function (err,register) {
+        res.json(register)
+    });
+
+
 }
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userController = require('../controller/userController');
-
 const router = express.Router();
 
 //router.post("/login", function (req, res) {
